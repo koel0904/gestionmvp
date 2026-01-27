0ba5b96971a090f8b525eb62d8641a6278a1acf3
koel0904 <koel090401@gmail.com>
2026-01-26 19:06:14 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import router from "./routes.js";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.use("/api", router);
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
